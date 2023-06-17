3aef487192c899a1c370cabcac0b8b150470d532
i1An <69718520+i1An@users.noreply.github.com>
2023-06-17 23:10:21 +0300

    Update nui.lua

--- client/handlers/nui.lua
+++ client/handlers/nui.lua
@@ -5,9 +5,17 @@ end)
 RegisterNuiCallback('onClick', function(data, cb)
     if not data.item.OnClick then return end
     if data.item.OnClick.Type == 'client-event' then
-        TriggerEvent(data.item.OnClick.Value)
+        if data.item.OnClick.Args then
+            TriggerEvent(data.item.OnClick.Value, table.unpack(data.item.OnClick.Args))
+        else
+            TriggerEvent(data.item.OnClick.Value)
+        end
     elseif data.item.OnClick.Type == 'server-event' then
-        TriggerServerEvent(data.item.OnClick.Value)
+        if data.item.OnClick.Args then
+            TriggerServerEvent(data.item.OnClick.Value, table.unpack(data.item.OnClick.Args))
+        else
+            TriggerServerEvent(data.item.OnClick.Value)
+        end
     elseif data.item.OnClick.Type == 'command' then
         ExecuteCommand(data.item.OnClick.Value)
     end
